4fb4ec6df0b280b2af5aeedbf745e0e40b4a290d
Septanesium <113310677+septanesium@users.noreply.github.com>
2024-08-22 16:04:15 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function loadLinks() {
+  let list = document.querySelector('ul#links');
+
+  let jsonLinks = JSON.parse('links.json').map(value => value.split(' & '));
+
+  for (let link of jsonLinks) {
+    let listItem = document.createElement('li');
+    let linkElement = document.createElement('button');
+
+    let url = link[1];
+
+    [linkElement.textContent, linkElement.onclick] =
+    [link[0], () => open(url, '_blank').focus() ];
+
+    listItem.appendChild(linkElement);
+    list.appendChild(listItem);
+  }
+}
+
+loadLinks();
