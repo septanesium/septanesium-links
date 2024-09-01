ff0fd61c30161b5e0b3c37324e1cb250878ebd99
Septanesium <113310677+septanesium@users.noreply.github.com>
2024-09-01 13:20:45 +0400

    Create links.js

---
+++ resources/links.js
@@ -0,0 +1,9 @@
+const SOCIAL_LINKS = [
+  'YouTube & https://www.youtube.com/@septanesium',
+  'TikTok & https://www.tiktok.com/@qabztract',
+  'GitHub & https://www.github.com/septanesium'
+];
+
+const PROJECT_LINKS = [
+  'Nothing Here Yet & https://www.youtube.com/watch?v=jKlMMJ4ic0E'
+];
