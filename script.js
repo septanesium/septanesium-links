ae07ae8434415830fcaccb334e1d96d29378afb6
Septanesium <113310677+septanesium@users.noreply.github.com>
2024-09-01 13:21:24 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,61 @@
-function loadLinks() {
-  let list = document.querySelector('ul#links');
+let tab = 0;
 
-  let jsonLinks = JSON_LINKS.map(value => value.split(' & '));
+const SOCIAL_LINKS = [
+  'YouTube & https://www.youtube.com/@septanesium',
+  'TikTok & https://www.tiktok.com/@qabztract',
+  'GitHub & https://www.github.com/septanesium',
+  'Geometry Dash & https://gdbrowser.com/u/23568051'
+];
+
+const PROJECT_LINKS = [
+  'Nothing Here Yet & https://www.youtube.com/watch?v=jKlMMJ4ic0E'
+];
+
+let elementLists = [
+  document.querySelector('ul#social-links'),
+  document.querySelector('ul#project-links')
+];
+
+function loadLinks() {
+  let linkLists = [ SOCIAL_LINKS, PROJECT_LINKS ];
+  let jsonLinks = linkLists[tab].map(value => value.split(' & '));
 
   for (let link of jsonLinks) {
     let listItem = document.createElement('li');
-    let linkElement = document.createElement('button');
+    let buttonElement = document.createElement('button');
 
     let url = link[1];
 
-    [linkElement.textContent, linkElement.onclick] =
-    [link[0], () => open(url, '_blank').focus() ];
+    [buttonElement.textContent, buttonElement.onclick] =
+    [link[0], () => open(url, '_blank').focus()];
 
-    listItem.appendChild(linkElement);
-    list.appendChild(listItem);
+    listItem.appendChild(buttonElement);
+    elementLists[tab].appendChild(listItem);
   }
+}
+
+function switchTabs(element) {
+  let setTabSwitchCases = {
+    'social-link-tab': 0,
+    'project-link-tab': 1
+  };
+
+  
+  document.querySelectorAll('.tab').forEach((e, index) => {
+    if (e.id != elementLists[index].id) {
+      elementLists[index].innerHTML = '';
+    }
+
+    e.classList
+    .remove('selected');
+  });
+
+  document.querySelector(`#${element.id}`).classList
+  .add('selected');
+
+  tab = setTabSwitchCases[element.id];
+
+  loadLinks();
 }
 
 loadLinks();
