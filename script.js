d694b4ba6fe3d4d84048da5fa0c75222095943d3
Septanesium <113310677+septanesium@users.noreply.github.com>
2024-09-01 13:25:54 +0400

    Update script.js
    lesson learned, if you're working with one file that splits into multiple on deployment make sure you don't redeclare a const :3

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 let tab = 0;
 
-const SOCIAL_LINKS = [
-  'YouTube & https://www.youtube.com/@septanesium',
-  'TikTok & https://www.tiktok.com/@qabztract',
-  'GitHub & https://www.github.com/septanesium',
-  'Geometry Dash & https://gdbrowser.com/u/23568051'
-];
-
-const PROJECT_LINKS = [
-  'Nothing Here Yet & https://www.youtube.com/watch?v=jKlMMJ4ic0E'
-];
-
 let elementLists = [
   document.querySelector('ul#social-links'),
   document.querySelector('ul#project-links')
