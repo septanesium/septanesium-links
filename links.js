4b54be14b59fb9341b16eef1dd0b16e82f210869
Septanesium <113310677+septanesium@users.noreply.github.com>
2024-09-01 13:13:24 +0400

    Update links.js
    refactoring and extra changes to the website

--- links.js
+++ links.js
@@ -1,5 +1,9 @@
-const JSON_LINKS = [
-    "YouTube & https://www.youtube.com/@septanesium",
-    "TikTok & https://www.tiktok.com/@qabztract",
-    "GitHub & https://www.github.com/septanesium"
-]
+const SOCIAL_LINKS = [
+  'YouTube & https://www.youtube.com/@septanesium',
+  'TikTok & https://www.tiktok.com/@qabztract',
+  'GitHub & https://www.github.com/septanesium'
+];
+
+const PROJECT_LINKS = [
+  'Nothing Here Yet & https://www.youtube.com/watch?v=jKlMMJ4ic0E'
+];
