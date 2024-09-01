863beaeb8fd6d48a37cc508c861713c30eac4ae2
Septanesium <113310677+septanesium@users.noreply.github.com>
2024-09-01 13:22:41 +0400

    Delete links.js

--- links.js
+++
@@ -1,9 +0,0 @@
-const SOCIAL_LINKS = [
-  'YouTube & https://www.youtube.com/@septanesium',
-  'TikTok & https://www.tiktok.com/@qabztract',
-  'GitHub & https://www.github.com/septanesium'
-];
-
-const PROJECT_LINKS = [
-  'Nothing Here Yet & https://www.youtube.com/watch?v=jKlMMJ4ic0E'
-];
